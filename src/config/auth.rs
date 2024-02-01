6fc207b8d0603fa9655f07c79e6a5948f0447e0f
Dhyey <dhpatel5021@gmail.com>
2024-02-01 10:46:33 +0530

    feat: added cors policy

--- src/config/auth.rs
+++ src/config/auth.rs
@@ -1,3 +1,8 @@
+use rocket::http::Method;
+use rocket_cors::{
+    AllowedHeaders, AllowedOrigins, Cors, CorsOptions, Error // 3.
+};
+
 pub const SECRET: &str = "secrets";
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
@@ -12,4 +17,27 @@ pub fn hash_password(password: String) -> String {
 
 pub fn verify_password(password: &str, hash: &str) -> bool {
     bcrypt::verify(password, hash).unwrap()
+}
+
+pub fn make_cors() -> Cors {
+    let allowed_origins = AllowedOrigins::some_exact(&[ // 4.
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://localhost:3000",
+        "http://0.0.0.0:8000",
+    ]);
+
+    CorsOptions { // 5.
+        allowed_origins,
+        allowed_methods: vec![Method::Get, Method::Post, Method::Put, Method::Delete].into_iter().map(From::from).collect(), // 1.
+        allowed_headers: AllowedHeaders::some(&[
+            "Authorization",
+            "Accept",
+            "Access-Control-Allow-Origin", // 6.
+        ]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS")
 }
